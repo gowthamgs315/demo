1590f52e08ef39695b3780b1dc9c81b994558a83
gowtham <gowthamgs315@gmail.com>
2022-07-18 07:44:27 +0530

    sprint02 commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,11 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!"); 
 		System.out.println("target ");
-<<<<<<< HEAD
-		System.out.println("target rember i have  ");
-=======
 		System.out.println("target rember i have to ");
 		System.out.println("got itghfg ");
 		System.out.println("got yytiuyit ");
 		System.out.println("got fghdghit ");
 		
->>>>>>> 8d0c56925c2ba584bbe3215bd762186c18eca3aa
         // Hello World!
     }
 }
